835a95ac937d749dec126ec69ddd25628bf15ee6
rwittler <roland.wittler@uni-bielefeld.de>
2023-03-02 16:42:12 +0100

    reformulated according to manuscript

--- canonical.c
+++ canonical.c
@@ -4,12 +4,29 @@
 #include <fasta.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <stdbool.h>
 
 
+// print a k-mer in binary format
+void printkmer(u_int64_t u){
+		// print kmer
+ 		u_int64_t t = pow(2, 63);   // t is the max number that can be represented
+	
+		for(t; t>0; t = t/2){       // t iterates through powers of 2
+			if(u >= t){             // check if u can be represented by current value of t
+				u -= t;
+				printf("1");        // if so, add a 1
+			}
+			else {
+				printf("0");        // if not, add a 0
+			}
+		}
+		
+		printf("\n");	
+}
 
-int process_string_std(char* s, int k, int* threads, int t){
-  
-
+// extract all k-mers, encode by standard 2-bit encoding, assign to b bins
+int process_string_std(char* s, int k, int* bins, int b){
 
 	u_int64_t kmer=0;
 	u_int64_t rckmer=0;
@@ -52,10 +69,10 @@ int process_string_std(char* s, int k, int* threads, int t){
 			
 			u_int64_t can = kmer < rckmer ? kmer : rckmer;
 			
-			// assign to thread
-			u_int64_t p = t * can / maxrank ;
+			// assign to bin
+			u_int64_t p = b * can / maxrank ;
 // 			int p = can % t ;
- 			threads[p]++;
+ 			bins[p]++;
 	   }
 
 	}
@@ -64,8 +81,8 @@ int process_string_std(char* s, int k, int* threads, int t){
 }
 
 
-
-int process_string(char* s, int k, int* threads, int t){
+// extract all k-mers, encode by enc^r_c, assign to b bins
+int process_string(char* s, int k, int* bins, int b){
 
 
 	
@@ -85,21 +102,26 @@ int process_string(char* s, int k, int* threads, int t){
 		posmasks[i]=posmasks[i+1]<<1;
 	}
 
+	// 1 .. 1 0 .. 0
 	u_int64_t onemasks[max+1];
 	onemasks[max]=allones;
 	for(int i=max-1; i>=0; i-- ){
 		onemasks[i]=(onemasks[i+1]<<1);
-		onemasks[i]=onemasks[i]&~(posmasks[offset+1]);
-		onemasks[i]=onemasks[i]|(posmasks[offset]);
 	}
 
-
+	// 0 .. 0 1 .. 1
 	u_int64_t zeromasks[max+1];
 	zeromasks[0]=allones;
 	for(int i=1; i<=max; i++ ){
 		zeromasks[i]=zeromasks[i-1]>>1;
 	}
 
+	// 0 .. 0 1 .. 1 0 .. 0
+	u_int64_t remaindermasks[k+1];
+	remaindermasks[0]=allones;
+	for(int i=1; i<=k; i++ ){
+		remaindermasks[i]=zeromasks[i+offset]&onemasks[max-i];
+	}
 	
 	u_int64_t maxrank = powl(2,2*k-1);
 	
@@ -109,138 +131,112 @@ int process_string(char* s, int k, int* threads, int t){
 		// this is not the actual max rank. It is chosen such that the bucket sizes are a bit smaller -- such that palindromic k-mers (which do not appear in pairs) contribute as much as the other canonical k-mers
 	}
 	
-	// * 0 A..A -> 00..0
-	// * 1 A..C -> 00..1
-	// * 2 A..G -> 01..0
-	// # 3 palindrome A..T -> 11..1
-	// * 4 C..A -> 01..1
-	// * 5 C..C -> 10..0
-	// # 6 palindrome C..G -> 11..0
-	// # 7 C..T -> A..G -> 01..0
-	// * 8 G..A -> 10..1
-	// # 9 palindrome G..C -> 00..1
-	// # 10 G..G -> C..C -> 10..0
-	// # 11 G..T -> A..C -> 00..1
-	// # 12 palindrome T..A -> 00..0
-	// # 13 T..C -> G..A -> 10..1
-	// # 14 T..G -> C..A -> 01..1
-	// # 15 T..T -> A..A -> 00..0
-	char replace1[16] = {0,1,0,1,1,0,0,0,1,1,0,1,0,1,1,0};
-	char replace2[16] = {0,0,1,1,1,0,1,1,0,0,0,0,0,0,1,0};
-	char replace3[16] = {0,0,0,1,0,1,1,0,1,0,1,0,0,1,0,0};
+
+	// * 0 A..A -> 0110
+	// * 1 A..C -> 0101
+	// * 2 A..G -> 0100
+	// # 3 palindrome A..T
+	// * 4 C..A -> 1000
+	// * 5 C..C -> 0111
+	// # 6 palindrome C..G
+	// # 7 C..T -> A..G -> 0100
+	// * 8 G..A -> 1001
+	// # 9 palindrome G..C
+	// # 10 G..G -> C..C -> 0111
+	// # 11 G..T -> A..C -> 0101
+	// # 12 palindrome T..A
+	// # 13 T..C -> G..A -> 1001
+	// # 14 T..G -> C..A -> 1000
+	// # 15 T..T -> A..A -> 0110
+	char replace1[16] = {0,0,0,0,1,0,0,0,1,0,0,0,0,1,1,0};
+	char replace2[16] = {1,1,1,0,0,1,0,1,0,0,1,1,0,0,0,1};
+	char replace3[16] = {1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1};
+	char replace4[16] = {0,1,0,0,0,1,0,0,1,0,1,1,0,1,0,0};
 	char reverse[16] =  {0,0,0,0,0,0,0,1,0,0,1,1,0,1,1,1};
 
+	// compute encoding where only setting the bits accorodung to specifying case and subtracting gaps is missing
+	u_int64_t initialhashed(u_int64_t *kmer, int offset, int l){
 
-	
-u_int64_t initialshortened(u_int64_t *kmer, int offset, int l){
-
-		const u_int64_t shiftedkmer = (*kmer)>>1;
-	
 		// pick a precomputed mask consisting of l trailing 1s and 0s else
 		// do and AND of that mask and the original k-mer to get the new right part
 		u_int64_t right = (*kmer) & zeromasks[offset+2*k-l]; //l trailing ones
 
-		// pick a precomputed mask consisting of a 0 , 1s, and l trailing 0s
-		// do and AND of that mask and the right-shifted k-mer to get the left part
-		u_int64_t left = shiftedkmer & onemasks[offset+2*k-l]; //l trailing zeros
-
-		// do and OR of  left (unshifted) and right (shifted) part
-		return (left | right);
-}
-
-
-u_int64_t shorten(u_int64_t *kmer, u_int64_t *rckmer){
-		   
+		// no remainder left?
+		if((l+2)>=k){ return(right); }
+		
+		// pick a precomputed mask consisting of ones in the middle to get the remainder
+		u_int64_t remainder = (*kmer) & remaindermasks[l+2];
+		
+		// shift remainder two bits to the right
+		remainder=remainder>>2;
 			
-			// shorten
-		   u_int64_t shortenedkmer;
-		   
-			// get length of symmetric pre/suffix
-			u_int64_t sym = (*kmer) ^ (*rckmer);
-			int l = __builtin_ctzll(sym)/2*2;
+		// do OR of left and middle part
+		return (remainder | right);
+	}
+
+	// compute enc_r_c
+	u_int64_t hash(u_int64_t *kmer, u_int64_t *rckmer){
+		
+		// hash
+		u_int64_t kmerhash;
+		
+		// get length of symmetric pre/suffix
+		u_int64_t sym = (*kmer) ^ (*rckmer);
+		int l = __builtin_ctzll(sym)/2*2;
+
+
+		if (l<k-1){ // not just single character in the middle
 			
-			// l<k-1 -> switch to inner encoding
-			// l==k-1 -> single charachter in the middle
-			// l==64 -> palindrome (set l to k-2!?)
-			
+			// get the first two asymmetric characters, i.e. 2x2 bits
 			char pattern=0;
+			if((*kmer)&posmasks[offset+l+1]){ pattern+=8;}
+			if((*kmer)&posmasks[offset+l+2]){ pattern+=4;}
+			if((*kmer)&posmasks[offset+2*k-l-1]){ pattern+=2;}
+			if((*kmer)&posmasks[offset+2*k-l]){ pattern+=1;}
 			
-			//palindrome. Set l such that coordinates match with usual "switch to inner" mode
-			if (l>k-1){l=k-2;} 
-
-			if (l<k-1){ // not just single character in the middle
-				
-				// get the first two asymmetric characters, i.e. 2x2 bits
-				if((*kmer)&posmasks[offset+l+1]){ pattern+=8;}
-				if((*kmer)&posmasks[offset+l+2]){ pattern+=4;}
-				if((*kmer)&posmasks[offset+2*k-l-1]){ pattern+=2;}
-				if((*kmer)&posmasks[offset+2*k-l]){ pattern+=1;}
-				
-				if(reverse[pattern]){
-					shortenedkmer = initialshortened(rckmer,offset,l);
-				} else {
-					shortenedkmer = initialshortened(kmer,offset,l);
-				}
-
-				// set positions l+2, l+3 and 2k-l according to *-pair-encoding
-				//setpos(&shortenedkmer,offsetl,offsetml,pattern);
-				if(replace1[pattern]){
-					shortenedkmer|=posmasks[offset+2*k-l];
-				} else {
-					shortenedkmer&=~posmasks[offset+2*k-l];
-				}
-				if(replace2[pattern]){
-					shortenedkmer|=posmasks[offset+l+3];
-				} else {
-					shortenedkmer&=~posmasks[offset+l+3];
-				}
-				if(replace3[pattern]){
-					shortenedkmer|=posmasks[offset+l+2];
-				} else {
-					shortenedkmer&=~posmasks[offset+l+2];
-				}
-
-			} else { // single character in the middle
-				
-				shortenedkmer = initialshortened(kmer,offset,l);
-			
-				// set the single bits according to encoding of *-case
-				//A,G -> do nothing
-				//C->1
-				if(!((*kmer)&posmasks[offset+l+1]) && (*kmer)&posmasks[offset+l+2]){
-					shortenedkmer|=posmasks[offset+2*k-l];
-				} else{
-					//T->0
-					if((*kmer)&posmasks[offset+l+1] && (*kmer)&posmasks[offset+l+2]){
-						shortenedkmer&=~posmasks[offset+2*k-l];
-					}
-				}
-				
-			} 
-
-			// pick a precomputed mask consisting of l+1 1s and then 0s
-			// OR with first mask to set 1s for left outer characters
-			shortenedkmer|=onemasks[offset+l+1];
-			shortenedkmer&=~onemasks[offset];
-
-			
-			// palindrome
-			if(pattern==9 || pattern==12){
-				// set first half to 0
-				shortenedkmer&=zeromasks[offset+k];
-				//  set very first bit to 1 to indicate palindrome
-				shortenedkmer|=posmasks[offset+1];
-				// for other palindromes (patterns 3 and 6), nothing has to be done
+			if(reverse[pattern]){
+				kmerhash = initialhashed(rckmer,offset,l);
+			} else {
+				kmerhash = initialhashed(kmer,offset,l);
 			}
 
-			return shortenedkmer;
-}
+			// set positions l+1, l+2, l+3 and l+4 according to *-pair-encoding
+			if(replace1[pattern]){ kmerhash|=posmasks[offset+l+1];}
+			if(replace2[pattern]){ kmerhash|=posmasks[offset+l+2];}
+			if(replace3[pattern]){ kmerhash|=posmasks[offset+l+3];}
+			if(replace4[pattern]){ kmerhash|=posmasks[offset+l+4];}
 
+			
+		} else if (l>=k) { // palindrome -> nothing to do
+			l=k;
+			kmerhash = initialhashed(kmer,offset,l);
+		} else { // single character in the middle
+			kmerhash = initialhashed(kmer,offset,l);
+			// set the bits accordingly
+			if((*kmer)&posmasks[offset+l+1]){ kmerhash|=~posmasks[offset+l+2];} //rc
+			if((*kmer)&posmasks[offset+l+2]){ kmerhash|=posmasks[offset+l+2];}
+		} 
+		
+		
+		// subtract gaps
+		// 2*(k//2-l-1) ones followed by k-2 zeros
+		if(l<=k-4){
+			u_int64_t gaps=zeromasks[max-(2*(k/2-l/2-1))];
+			gaps=gaps<<(2*((k+1)/2)-1);
+			kmerhash-=gaps;
+		}
 
-	
-	
-	
+		// subtract gap in code due to specifying middle position
+		if (k%2==1 && kmerhash>=pow(4,(k/2+1))){
+			kmerhash -= 2*pow(4,k/2);
+		}
+		
+		return kmerhash;
+	}
+
+	// go through the string, process each k-mer by shift/update, enc_r_c, and assign to bins
 	for (int i=0;i<strlen(s);i++){
+		
 
 		//update kmer by current character
 		kmer=kmer<<2;
@@ -251,7 +247,7 @@ u_int64_t shorten(u_int64_t *kmer, u_int64_t *rckmer){
 			case 'T': kmer+=3; break;
 			default: if(!warned) {fprintf(stderr, "Warning: unsupported character replaced by A: %c\n", s[i]); warned=1; } break;
 		}
-
+		
 		// clear unused bits
  		kmer &= ~posmasks[offset];
  		kmer &= ~posmasks[offset-1];
@@ -270,21 +266,20 @@ u_int64_t shorten(u_int64_t *kmer, u_int64_t *rckmer){
 
 	   if (i>=k-1){
 		   
-			// shorten
-		    u_int64_t shortenedkmer = shorten(&kmer, &rckmer);
+	   
+			// hash
+		    u_int64_t kmerhash = hash(&kmer, &rckmer);
 			
-			// assign to thread
-			u_int64_t p = t*shortenedkmer/maxrank;
-
-			if (p==t) {
-				// palindromes comes not in pairs like the other canonical k-mers. In order to ensure a equal distribution, bucket sizes are chosen as if there were only half as many palindromes possible and the other palindromes are assigned to the last bucket
-				threads[p-1]++;
+			// assign to bin
+			u_int64_t p = b*kmerhash/maxrank;
+			
+			if (p==b) {
+				// palindromes comes not in pairs like the other canonical k-mers. In order to ensure a equal distribution, bucket sizes are chosen as if there were only half as many palindromes possible. This results in ranks that would correspond to a max+1st bin. These k-mers are assigned to bin 0 where the palindromes are "missing".
+				bins[0]++;
 			} else {
-				threads[p]++;
+				bins[p]++;
 			}
 				
-//    		printf("%llu\n",p);
-//  		printf("%llu\n",shortenedkmer);
 			
 		}
 
@@ -298,11 +293,11 @@ int main(int argc, char* argv[]) {
    
 	// default values
 	int k=5; // k-mer length
-	int t=4; // number of threads/buckets to assign canonical k-mers to
+	int b=4; // number of bins/buckets to assign canonical k-mers to
 
 	// parse arguments
 	if (argc<2){
-		fprintf(stderr, "arguments: fasta file, k (default 5, smaller 32), t (default 4)");
+		fprintf(stderr, "arguments: fasta file, k (default 5, smaller 32), bin number (default 4)");
 		exit(1);
 	}
 	
@@ -315,15 +310,15 @@ int main(int argc, char* argv[]) {
 	}
 	
 	if (argc>3){
-		t=atoi(argv[3]);
+		b=atoi(argv[3]);
 	}
 
 
 	
 	// initialize array
-	int threads[t];
-	for (int i=0;i<t;i++){
-		threads[i]=0;
+	int bins[b];
+	for (int i=0;i<b;i++){
+		bins[i]=0;
 	}
 	
 	/* process FASTA file */
@@ -338,11 +333,11 @@ int main(int argc, char* argv[]) {
 		if (
 		
 			//***
-			//*** distribute canonical k-mers to threads
+			//*** distribute canonical k-mers to bins
 			//***
 			
-			process_string(seq,k,threads,t)
-// 			process_string_std(seq,k,threads,t)
+			process_string(seq,k,bins,b)
+// 			process_string_std(seq,k,bins,b)
 			
 		){exit(1);}
 
@@ -351,16 +346,14 @@ int main(int argc, char* argv[]) {
 	}
 	CloseFASTA(ffp);
   
-
-//  	exit(0);
 	
-	// output distribution to threads
+	// output distribution to bins
 	int sum=0;
-	for (int i=0;i<t;i++){
- 		printf("%d ",threads[i]);
-		sum+=threads[i];
+	for (int i=0;i<b;i++){
+ 		printf("%d\n",bins[i]);
+		sum+=bins[i];
 	}
- 	printf("\nSUM: %d\n",sum);
+//  	printf("\nSUM: %d\n",sum);
    
 	return 0;
    
